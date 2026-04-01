2beec428669dfe149fe4e1edae13f9f999ad7605
Rakesh Madu <rakeshmadu24@gmail.com>
2026-04-01 22:53:48 +0530

    updated

--- productivity_app/urls.py
+++ productivity_app/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 
-    # custom auth paths
+    # customauth paths
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
