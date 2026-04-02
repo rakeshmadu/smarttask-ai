67b3e553e57452adf2c39800b25d8a38821ab410
Rakesh Madu <rakeshmadu24@gmail.com>
2026-04-02 10:49:19 +0530

    signup update

--- core/views.py
+++ core/views.py
@@ -3,7 +3,18 @@ from django.contrib.auth.decorators import login_required
 from .models import Task
 from .forms import TaskForm
 import random
+from django.contrib.auth.forms import UserCreationForm
 
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'signup.html', {'form': form})
 @login_required
 def ai_suggestions(request):
     suggestions = [
--- productivity_app/urls.py
+++ productivity_app/urls.py
@@ -18,12 +18,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-
+from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 
     # customauth paths
+    path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
